07ca62e646280c471cc055e0034f2aa3aa8a68bc
songxiangyu1994 <songxiangyu1994@163.com>
2019-05-20 18:13:02 +0800

    修改Controller层代码

--- src/main/java/top/gnibbuq/controller/AdminController.java
+++ src/main/java/top/gnibbuq/controller/AdminController.java
@@ -1,9 +1,63 @@
 package top.gnibbuq.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import top.gnibbuq.pojo.ResultInfo;
+import top.gnibbuq.pojo.User;
+import top.gnibbuq.service.AdminService;
 
 @RestController
-@RequestMapping
+@RequestMapping("admin_user")
 public class AdminController {
+    @Autowired
+    private AdminService adminService;
+    @RequestMapping(value = "addUser" )
+    @ResponseBody
+    public String addUser(User user, @RequestParam("username")String username){
+        String jsonData = null;
+        try {
+            jsonData = adminService.addUser();
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResultInfo resultInfo = new ResultInfo(false,null,"服务器忙，请稍后再试！");
+            //将resultInfo转换为String
+            try {
+                jsonData = new ObjectMapper().writeValueAsString(resultInfo);
+            } catch (JsonProcessingException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+        return jsonData;
+    }
+
+
+    @RequestMapping(value = "queryByUsername" )
+    @ResponseBody
+    public String queryByUsername(@RequestParam("username")String username ){
+        String jsonData = null;
+        try {
+            jsonData = adminService.queryByUsername();
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResultInfo resultInfo = new ResultInfo(false,null,"服务器忙，请稍后再试！");
+            //将resultInfo转换为String
+            try {
+                jsonData = new ObjectMapper().writeValueAsString(resultInfo);
+            } catch (JsonProcessingException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+        return jsonData;
+    }
+
+
+
+
 }
---
+++ src/main/java/top/gnibbuq/service/AdminService.java
@@ -0,0 +1,7 @@
+package top.gnibbuq.service;
+
+public interface AdminService {
+    String addUser();
+
+    String queryByUsername();
+}
