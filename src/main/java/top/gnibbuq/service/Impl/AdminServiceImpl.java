9af9e8d6a75d14f4e89e9d053e90927c80bc77d1
songxiangyu1994 <songxiangyu1994@163.com>
2019-05-21 10:28:41 +0800

    更新

--- src/main/java/top/gnibbuq/service/Impl/AdminServiceImpl.java
+++ src/main/java/top/gnibbuq/service/Impl/AdminServiceImpl.java
@@ -1,7 +1,24 @@
 package top.gnibbuq.service.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import top.gnibbuq.pojo.User;
 import top.gnibbuq.service.AdminService;
 
-public class AdminServiceImpl implements AdminService {
+import java.util.List;
 
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminMapper adminMapper;
+    @Override
+    public String queryByUsername(User user) {
+        List<User> userList = adminMapper.queryByUsername(user);
+        
+        return null;
+    }
+
+    @Override
+    public String addUser(User user) {
+
+        return null;
+    }
 }
