e6a9f19680fa4d5dc6c5a123536a414a6624483a
songxiangyu1994 <songxiangyu1994@163.com>
2019-07-14 13:22:15 +0800

    查询商品完结

--- src/main/java/top/gnibbuq/service/Impl/CommodityServiceImpl.java
+++ src/main/java/top/gnibbuq/service/Impl/CommodityServiceImpl.java
@@ -1,14 +1,20 @@
 package top.gnibbuq.service.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import top.gnibbuq.mapper.CommodityMapper;
 import top.gnibbuq.pojo.Commodity;
 import top.gnibbuq.service.CommodityService;
 
 import java.util.List;
 @Service
 public class CommodityServiceImpl implements CommodityService {
+	@Autowired
+	private CommodityMapper commodityMapper;
     @Override
     public List<Commodity> getAllCommodity() {
-        return null;
+    	List<Commodity> list = commodityMapper.getAllCommodity();
+        return list;
     }
 }
